cbe87443f6b6eb1c6ec5a0266e56f6689295a62c
Josue Morales <thejosuescript@gmail.com>
2023-09-01 15:09:15 -0600

    checkers up [binary, NaNdIgits, digitsOutOfRange]

--- src/baseChecker.tool.js
+++ src/baseChecker.tool.js
@@ -12,20 +12,33 @@ function baseChecker(number, radix) {
         })
         throw baseError
     }
+    //checker binary
+    if (radix === 2) {
+        /**@type {string} */
+        const digits = !number.decimals ? number.ints : number.ints + number.decimals
+        if (digits.split('').some(digit => digit !== '0' && digit !== '1')) {
+            const invalidBinary = new customError('invalidBinary', 'If a number is from base 2, it cannot have different digits to 0 or 1', {
+                invalidDigits: digits.split('').filter(digit => digit !== '0' && digit !== '1'),
+                invalidNumber: number.decimals ? number.ints + '.' + number.decimals : number.ints
+            })
+            throw invalidBinary
+        }
+    }
+    //checker NaN digits in bases less than 11
     if (!number.decimals) {
-        if (radix < 10 && number.ints.split('').some(digit => {
+        if (radix < 11 && number.ints.split('').some(digit => {
             if (digit !== '.' && isNaN(digit)) {
                 return true
             } else return false
         })) {
-            const numberError = new customError('numberError', 'Numbers with NaN digits in bases less than 10 such as decimal, octal and binary are not allowed.', {
+            const numberError = new customError('numberError', 'Numbers with NaN digits in bases less than 11 such as decimal, octal and binary are not allowed.', {
                 currentBase: radix,
-                currentNotAllowedNumber: number
+                currentNotAllowedNumber: number.decimals ? number.ints + '.' + number.decimals : number.ints
             })
             throw numberError
         }
     } else {
-        if (radix<10) {
+        if (radix < 11) {
             if (number.ints.split('').some(digit => {
                 if (digit !== '.' && isNaN(digit)) {
                     return true
@@ -35,14 +48,54 @@ function baseChecker(number, radix) {
                     return true
                 } else return false
             })) {
-                const numberError = new customError('numberError', 'Numbers with NaN digits in bases less than 10 such as decimal, octal and binary are not allowed.', {
+                const numberError = new customError('numberError', 'Numbers with NaN digits in bases less than 11 such as decimal, octal and binary are not allowed.', {
                     currentBase: radix,
-                    currentNotAllowedNumber: number
+                    currentNotAllowedNumber: number.decimals ? number.ints + '.' + number.decimals : number.ints
                 })
                 throw numberError
             }
         }
     }
+    //checker numeric digits out of range in bases less than 11
+    if (radix<11) {
+        /**@type {string} */
+        const digits = !number.decimals ? number.ints : number.ints + number.decimals
+        if (digits.split('').some(digit=>Number(digit)>=Number(radix))) {
+            const invalidDigit= new customError('invalidDigit',`you are using a base of ${radix} and you have digits in your number greater than the maximum digit allowed for your base`,{
+                maximumDigitAllowed:radix-1,
+                invalidDigits:digits.split('').filter(digit=>digit>=radix)
+            })
+            throw invalidDigit
+        }
+    }
+    //checker NaN digits out of range in bases greater than 10
+    if (radix>10) {
+        /**@type {string} */
+        const digits = !number.decimals ? number.ints : number.ints + number.decimals
+        if (digits.split('').some(digit=>{
+            if (isNaN(digit)) {
+                const numericDigit=digit.toUpperCase().charCodeAt(0)-55
+                return numericDigit>=Number(radix)
+            }else{
+                return Number(digit)>=Number(radix)
+            }
+        })) {
+            const invalidDigit= new customError('invalidDigit',`you are using a base of ${radix} and you have digits in your number greater than the maximum digit allowed for your base`,{
+                maximumDigitAllowed:String.fromCharCode((radix-1)+55),
+                invalidDigits:digits.split('').filter(digit=>{
+                    if (isNaN(digit)) {
+                        const numericDigit=digit.toUpperCase().charCodeAt(0)-55
+                        return numericDigit>=Number(radix)
+                    }else{
+                        return Number(digit)>=Number(radix)
+                    }
+                })
+            })
+            throw invalidDigit
+        }
+    }
+
+    
 }
 
 module.exports = baseChecker
